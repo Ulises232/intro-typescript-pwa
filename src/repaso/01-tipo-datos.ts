eb0c6d9864744bf606cb90b6e5b4d3a1f0bfec3a
Ulises232 <ldaulises666@gmail.com>
2022-09-10 09:41:11 -0500

    Practica uno

---
+++ src/repaso/01-tipo-datos.ts
@@ -0,0 +1,15 @@
+
+console.log('Hola Mundo - pwd-1003!');
+
+let nombre : String = "Ulises";
+let cont:  number = 111;
+let acredita : boolean = false;
+
+let apaterno : number | string = 'juan';
+
+apaterno = 238;
+
+
+var alVariable : string = "hola";
+
+const tem : any = 234;
